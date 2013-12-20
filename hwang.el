b225e22153fdde985df0187ec3e957dee5258b6a
Henry Wang <wh_henry@hotmail.com>
2013-12-20 23:16:49 +1100

    use tilde for home directory

--- hwang.el
+++ hwang.el
@@ -313,7 +313,9 @@
    "%s@%s:%s%s "
    (user-login-name)
    (car (split-string (system-name) "\\."))
-   (if (= (length default-directory) 1) default-directory (substring default-directory 0 -1))
+   (replace-regexp-in-string
+    (format "^%s" (getenv "HOME")) "~"
+    (if (= (length default-directory) 1) default-directory (substring default-directory 0 -1)))
    (if (= (user-uid) 0) "#" "$"))
   )
 (setq eshell-prompt-function 'hwang/eshell-prompt)
