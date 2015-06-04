fd9477076dadec70f0cd4278257ea7bc2b96c9fc
Henry Wang <wh_henry@hotmail.com>
2015-06-04 12:15:51 +1000

    set up org-capture

--- lisp/mods/org.el
+++ lisp/mods/org.el
@@ -2,19 +2,39 @@
 ;; Org mode setup
 ;;
 
-;(require 'ox-latex)
-
-;(add-to-list 'org-latex-packages-alist '("" "minted"))
-
-;(setq org-latex-listings 'minted)
-
 (setq org-directory "~/org")
 (setq org-src-fontify-natively t)
-(setq org-default-notes-file (concat org-directory "/notes.org"))
 
-;(setq org-latex-pdf-process
-;      '("pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
-;        "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
-;        "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
+(defun hwang-journal-file-name ()
+  (let ((day (format-time-string "%Y-%m")))
+    (concat "~/org/journal-" day ".org")
+    )
+  )
+
+(setq org-default-notes-file (hwang-journal-file-name))
+
+(setq
+ org-capture-templates
+ '(
+   ("t" "Todo" entry (file+headline "~/org/todo.org" "Tasks")
+    "* TODO %?\n  %i\n  %a")
+   ("g" "General" entry (file+headline (hwang-journal-file-name) "General")
+    "* %?\nEntered on %U\n  %i\n  %a")
+   ("d" "Diary" entry (file+headline (hwang-journal-file-name) "Diary")
+    "* %?\nEntered on %U\n  %i\n  %a")
+   ("p" "Programming" entry (file+headline (hwang-journal-file-name) "Programming")
+    "* %?\nEntered on %U\n  %i\n  %a")
+   ("x" "Linux" entry (file+headline (hwang-journal-file-name) "Linux")
+    "* %?\nEntered on %U\n  %i\n  %a")
+   ("k" "Networking" entry (file+headline (hwang-journal-file-name) "Network")
+    "* %?\nEntered on %U\n  %i\n  %a")
+   ("k" "Graphics" entry (file+headline (hwang-journal-file-name) "Graphics")
+    "* %?\nEntered on %U\n  %i\n  %a")
+   ("w" "Windows" entry (file+headline (hwang-journal-file-name) "Windows")
+    "* %?\nEntered on %U\n  %i\n  %a")
+   ("o" "Work" entry (file+headline (hwang-journal-file-name) "Work")
+    "* %?\nEntered on %U\n  %i\n  %a")
+   )
+ )
 
 (provide 'mods/org)
