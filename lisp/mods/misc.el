15ea25e26d2886ec7af0980f7bc6d77b62f57149
Henry Wang <henwang@amazon.com>
2018-07-25 15:23:43 -0700

    configure ispell

--- lisp/mods/misc.el
+++ lisp/mods/misc.el
@@ -77,4 +77,20 @@
 (setq projectile-project-search-path '("~/devel/"))
 (setq projectile-enable-caching t)
 
+;; Customize iSpell
+(cond
+ ((executable-find "hunspell")
+  (setq ispell-program-name "hunspell")
+  (setq ispell-local-dictionary "en_US")
+  (setq ispell-local-dictionary-alist
+        ;; Please note the list `("-d" "en_US")` contains ACTUAL parameters passed to hunspell
+        ;; You could use `("-d" "en_US,en_US-med")` to check with multiple dictionaries
+        '(("en_US" "[[:alpha:]]" "[^[:alpha:]]" "[']" nil ("-d" "en_US") nil utf-8)
+          )))
+
+ ((executable-find "aspell")
+  (setq ispell-program-name "aspell")
+  ;; Please note ispell-extra-args contains ACTUAL parameters passed to aspell
+  (setq ispell-extra-args '("--sug-mode=ultra" "--lang=en_US"))))
+
 (provide 'mods/misc)
