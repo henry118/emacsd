85f132d9495a90d97ebef700f9d62075a193b6e1
Henry Wang <henwang@amazon.com>
2020-08-07 20:52:42 -0700

    Added rust mode setup instructions

--- lisp/mods/rust.el
+++ lisp/mods/rust.el
@@ -1,6 +1,15 @@
 ;;
 ;; Rust mode setup
 ;;
+;; Instruction
+;; ===========
+;; 1. curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+;; 2. export PATH="$PATH:$HOME/.cargo/bin"
+;; 3. rustup toolchain add nightly
+;; 4. cargo +nightly install racer
+;; 5. rustup component add rust-src
+;; 6. export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
+;;
 
 (require 'rust-mode)
 
