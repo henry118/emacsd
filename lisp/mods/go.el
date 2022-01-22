682eb4e9a54ff8fc3b28be1e364c07b18ae1a13c
Henry Wang <wh_henry@hotmail.com>
2022-01-21 23:58:36 -0800

    golang: switch back to imenu

--- lisp/mods/go.el
+++ lisp/mods/go.el
@@ -29,7 +29,7 @@
   (local-set-key (kbd "M-,")   'pop-tag-mark)
   (local-set-key (kbd "C-,")   'lsp-find-references)
   (local-set-key (kbd "C-.")   'lsp-goto-implementation)
-  (local-set-key (kbd "M-m")   'lsp-ui-imenu)
+  (local-set-key (kbd "M-m")   'helm-semantic-or-imenu)
 
   ; Customize compile command to run go build
   (if (not (string-match "go" compile-command))
